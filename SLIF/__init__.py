5e43bd1030db5114c501e560f5c68f7c65e65915
alan-hamm <daimones@proton.me>
2024-11-02 22:18:08 -0400

    remove obsolete data_io.py
    Signed-off-by: alan-hamm <daimones@proton.me>

--- SLIF/__init__.py
+++ SLIF/__init__.py
@@ -1,28 +1,24 @@
-# Developed traditionally with the addition of AI assistance
-# Author: Alan Hamm (pqn7)
+# __init__.py - Initialization for SLIF Package
+# Author: Alan Hamm
 # Date: April 2024
 
-# Import key functions and classes from submodules to allow easy access at the package level
-from .data_io import get_num_records, futures_create_lda_datasets
-from .utils import garbage_collection,  exponential_backoff, convert_float32_to_float
-from .logging_helper import archive_log, close_logger, setup_logging
-from .process_futures import retry_processing, process_completed_futures, handle_failed_future
+# Description:
+# This __init__.py file initializes the Scalable LDA Insights Framework (SLIF) package.
+# It exposes key functions and classes from various submodules to simplify access
+# at the package level, providing a cohesive interface for the framework.
 
+# Import essential functions and classes from submodules
+from .utils import garbage_collection, exponential_backoff, convert_float32_to_float
+from .logging_helper import archive_log, close_logger, setup_logging
+from .process_futures import process_completed_futures, futures_create_lda_datasets
 from .model import train_model
 from .alpha_eta import calculate_numeric_alpha, calculate_numeric_beta, validate_alpha_beta, calculate_alpha_beta
 from .visualization import create_vis_pylda, create_vis_pcoa
-
 from .write_to_postgres import save_to_zip, create_dynamic_table_class, create_table_if_not_exists, add_model_data_to_database
-
 from .yaml_loader import join, getenv, get_current_time
 
-
-# Define what should be imported with "from my_lda_library import *"
+# Define __all__ to control what is imported with "from SLIF import *"
 __all__ = [
-    # data_io
-    'get_num_records',
-    'futures_create_lda_datasets',
-
     # utils
     'garbage_collection',
     'exponential_backoff',
@@ -52,9 +48,8 @@ __all__ = [
     'create_vis_pcoa',
 
     # process_futures
-    'retry_processing',
     'process_completed_futures',
-    'handle_failed_future',
+    'futures_create_lda_datasets',
 
     # writeToPostgres
     'save_to_zip', 
