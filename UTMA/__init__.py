71811450484f50744106659b6d01801e24a6f0c5
alan-hamm <daimones+git@proton.me>
2024-11-05 09:04:31 -0500

    remove dead scripts
    Signed-off-by: alan-hamm <daimones+git@proton.me>

--- UTMA/__init__.py
+++ UTMA/__init__.py
@@ -1,4 +1,4 @@
-# __init__.py - Initialization for SLIF Package
+# __init__.py - Initialization for UTMA Package
 # Author: Alan Hamm
 # Date: April 2024
 
@@ -9,7 +9,6 @@
 
 # Import essential functions and classes from submodules
 from .utils import garbage_collection, exponential_backoff, convert_float32_to_float
-from .logging_helper import archive_log, close_logger, setup_logging
 from .process_futures import process_completed_futures, futures_create_lda_datasets
 from .topic_model_trainer import train_model_v2
 from .alpha_eta import calculate_numeric_alpha, calculate_numeric_beta, validate_alpha_beta, calculate_alpha_beta
@@ -17,17 +16,12 @@ from .visualization import create_vis_pylda, create_vis_pcoa, process_visualizat
 from .write_to_postgres import save_to_zip, create_dynamic_table_class, create_table_if_not_exists, add_model_data_to_database
 from .yaml_loader import join, getenv, get_current_time
 
-# Define __all__ to control what is imported with "from SLIF import *"
+# Define __all__ to control what is imported with "from UTMA import *"
 __all__ = [
     # utils
     'garbage_collection',
     'exponential_backoff',
     'convert_float32_to_float',
-
-    #logging_helper
-    'archive_log', 
-    'close_logger', 
-    'setup_logging',
 
     #yaml_loader
     'join', 
