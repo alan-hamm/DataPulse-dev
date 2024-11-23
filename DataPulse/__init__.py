9bdab9af5208914b49037a2e8acca5663759e41c
alan-hamm <daimones+git@proton.me>
2024-11-22 19:52:42 -0500

    add functions cpu_simulate_coherence_scores_with_lln, gpu_simulate_coherence_scores_with_lln, simulate_coherence_scores_with_lln_optimized, estimate_futures_batches_large_optimized', futures_create_lda_datasets_v3
    Signed-off-by: alan-hamm <daimones+git@proton.me>

--- DataPulse/__init__.py
+++ DataPulse/__init__.py
@@ -9,7 +9,7 @@
 
 # Import essential functions and classes from submodules
 from .utils import *
-from .process_futures import process_completed_futures, futures_create_lda_datasets, futures_create_lda_datasets_v2, get_and_process_show_topics, get_document_topics_batch, extract_topics_with_get_topic_terms
+from .process_futures import *
 from .topic_model_trainer import train_model_v2
 from .alpha_eta import calculate_numeric_alpha, calculate_numeric_beta, validate_alpha_beta, calculate_alpha_beta
 from .visualization import create_vis_pylda, create_vis_pcoa, process_visualizations, create_vis_pca, create_tsne_plot, get_document_topics
@@ -17,7 +17,7 @@ from .write_to_postgres import save_to_zip, create_dynamic_table_class, create_t
 from .yaml_loader import join, getenv, get_current_time
 from .postgres_logging  import  PostgresLoggingHandler
 from .mathstats import *
-from .batch_estimation import estimate_futures_batches, estimate_futures_batches_large_docs, estimate_futures_batches_large_docs_v2
+from .batch_estimation import estimate_futures_batches, estimate_futures_batches_large_docs, estimate_futures_batches_large_docs_v2, estimate_futures_batches_large_optimized
 
 # Define __all__ to control what is imported with "from SpectraSync import *"
 __all__ = [
@@ -34,11 +34,15 @@ __all__ = [
     'calculate_convergence',
     'calculate_perplexity_score',
     'calculate_value',
+    'cpu_simulate_coherence_scores_with_lln',
+    'gpu_simulate_coherence_scores_with_lln',
+    'simulate_coherence_scores_with_lln_optimized',
 
     # batch estimation
     'estimate_futures_batches_large_docs',
     'estimate_futures_batches',
     'estimate_futures_batches_large_docs_v2',
+    'estimate_futures_batches_large_optimized',
 
     # utils
     'garbage_collection',
@@ -79,6 +83,7 @@ __all__ = [
     'process_completed_futures',
     'futures_create_lda_datasets',
     'futures_create_lda_datasets_v2',
+    'futures_create_lda_datasets_v3',
     'get_and_process_show_topics',
     'extract_topics_with_get_topic_terms',
 
